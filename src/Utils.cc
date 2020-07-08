d98c1336df4ca5bcaa9bf644df296d077226b17e
gosonzhang <gosonzhang@tencent.com>
2020-07-08 21:15:50 +0800

    [TUBEMQ-269] Create C/C++ RmtDataCache class

--- src/Utils.cc
+++ src/Utils.cc
@@ -31,6 +31,9 @@
 
 namespace tubemq {
 
+using std::stringstream;
+
+
 static const string kWhitespaceCharSet = " \n\r\t\f\v";
 
 string Utils::Trim(const string& source) {
@@ -270,3 +273,4 @@ int64_t Utils::GetCurrentTimeMillis() {
 }
 
 }  // namespace tubemq
+
