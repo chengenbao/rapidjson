d0fb773ed639e1bcebeef01e51337ac121f310f4
charleli <charleli@tencent.com>
2020-09-14 12:06:51 +0800

    增加打印

--- src/tubemq_codec.h
+++ src/tubemq_codec.h
@@ -185,9 +185,10 @@ class TubeMQCodec final : public CodecProtocol {
   // return code: -1 failed; 0-Unfinished; > 0 package buffer size
   virtual int32_t Check(BufferPtr &in, Any &out, uint32_t &request_id, bool &has_request_id,
                         size_t &package_length) {
+    LOG_TRACE("check in:%s", in->String().c_str());
     // check package is valid
     if (in->length() < 12) {
-      //package_length = 12;
+      // package_length = 12;
       LOG_TRACE("Check: data's length < 12, is %ld, out", in->length());
       return 0;
     }
@@ -206,10 +207,10 @@ class TubeMQCodec final : public CodecProtocol {
     }
     // check data list
     uint32_t item_len = 0;
-    //package_length = 12;
+    // package_length = 12;
     auto check_buf = in->Slice();
     for (uint32_t i = 0; i < list_size; i++) {
-      //package_length += 4;
+      // package_length += 4;
       if (check_buf->length() < 4) {
         LOG_TRACE("Check: buffer Remaining length < 4, is %ld, out", check_buf->length());
         return 0;
@@ -224,7 +225,7 @@ class TubeMQCodec final : public CodecProtocol {
                   rpc_config::kRpcMaxBufferSize);
         return -1;
       }
-      //package_length += item_len;
+      // package_length += item_len;
       if (item_len > check_buf->length()) {
         LOG_TRACE("Check: item_len(%d) > remaining length(%ld), out", item_len,
                   check_buf->length());
@@ -243,8 +244,8 @@ class TubeMQCodec final : public CodecProtocol {
       in->Skip(item_len);
     }
     out = buf;
-    LOG_TRACE("Check: received message check finished, request_id=%d, readed_len:%d",
-      request_id, readed_len);
+    LOG_TRACE("Check: received message check finished, request_id=%d, readed_len:%d", request_id,
+              readed_len);
     return readed_len;
   }
 
@@ -305,8 +306,7 @@ class TubeMQCodec final : public CodecProtocol {
     }
     return block_cnt;
   }
-
- };
+};
 }  // namespace tubemq
 #endif
 
