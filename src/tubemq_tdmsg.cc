3e4a3301841c3ef6852364a9dbc6f6a4b0516a6c
gosonzhang <gosonzhang@tencent.com>
2020-11-20 15:46:57 +0800

    直接使用vecotr的内容

--- src/tubemq_tdmsg.cc
+++ src/tubemq_tdmsg.cc
@@ -193,8 +193,6 @@ bool TubeMQTDMsg::ParseTDMsg(const char* data, uint32_t data_length, string& err
 
 bool TubeMQTDMsg::ParseTDMsg(const vector<char>& data_vec, string& err_info) {
   bool result = false;
-  char* tmp_data = NULL;
-  uint32_t tmp_length = 0;
 
   if (is_parsed_) {
     err_info = "TDMsg is parsed, if need re-parse, please clear first!";
@@ -204,15 +202,7 @@ bool TubeMQTDMsg::ParseTDMsg(const vector<char>& data_vec, string& err_info) {
       err_info = "Check parameter data is NULL or data_length is zero!";
       return result;
   }
-  tmp_length = data_vec.size();
-  tmp_data = new char[tmp_length + 1];
-  memset(tmp_data, 0, tmp_length + 1);
-  for (uint32_t i = 0; i < data_vec.size(); i++) {
-    tmp_data[i] = data_vec[i];
-  }
-  result = ParseTDMsg(tmp_data, tmp_length, err_info);
-  delete[] tmp_data;
-  tmp_data = NULL;
+  result = ParseTDMsg(data_vec.data(), data_vec.size(), err_info);
   return result;
 }
 
